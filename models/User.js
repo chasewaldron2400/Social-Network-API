f69698efe059d82897c7113eeefc11d1dd3a2636
chasewaldron2400 <chasew2400@gmail.com>
2024-11-09 22:36:55 -0600

    user model

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/\S+@\S+\.\S+/, 'chasew2400@gmail.com']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+          },
+          id: false,
+        }
+      );
